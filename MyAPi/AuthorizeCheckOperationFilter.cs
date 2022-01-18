d6bf5c910d9be99bafa5b97bbd81a49f2fcbf309
Eric <outfoxed@163.com>
2022-01-18 17:47:34 +0800

    Add Swagger Identityserver4 auth

---
+++ MyAPi/AuthorizeCheckOperationFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+namespace MyAPi
+{
+    public class AuthorizeCheckOperationFilter : IOperationFilter
+    {
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+
+        public AuthorizeCheckOperationFilter(Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var hasAuthorize = context.MethodInfo.DeclaringType != null && (context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any()
+                                                                            || context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any());
+
+            if (hasAuthorize)
+            {
+                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+                operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+
+                operation.Security = new List<OpenApiSecurityRequirement>
+                {
+                    new OpenApiSecurityRequirement
+                    {
+                        [
+                            new OpenApiSecurityScheme {Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "oauth2"}
+                            }
+                        ] = new string[] { _configuration["SMEApiConfiguration:OidcApiName"] }
+                    }
+                };
+
+            }
+        }
+    }
+}
--- MyAPi/Startup.cs
+++ MyAPi/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,8 +7,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,9 +30,50 @@ namespace MyAPi
         {
 
             services.AddControllers();
+
+            //使用Bearer
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = Configuration["ApiConfiguration:IdentityServerBaseUrl"];
+                    options.RequireHttpsMetadata = Convert.ToBoolean(Configuration["ApiConfiguration:RequireHttpsMetadata"] ?? "false");
+                    options.Audience = Configuration["ApiConfiguration:OidcApiName"];
+                });
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyA", Version = "v1" });
+
+                c.OperationFilter<AuthorizeCheckOperationFilter>(); // IdentityServer4认证过滤
+                // 开启鉴权小锁
+                c.OperationFilter<AddResponseHeadersFilter>();
+                c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        AuthorizationCode = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri($"{Configuration["ApiConfiguration:IdentityServerBaseUrl"]}/connect/authorize"),
+                            TokenUrl = new Uri($"{Configuration["ApiConfiguration:IdentityServerBaseUrl"]}/connect/token"),
+                            Scopes = new Dictionary<string, string> {
+                                {
+                                    Configuration.GetSection("ApiConfiguration:ApiScopesForClientCredentials")?.Get<string[]>()?[0],
+                                    "my ApiScopesForClientCredentials"
+                                },
+                            }
+                        }
+                    }
+                });
             });
         }
 
@@ -39,12 +83,28 @@ namespace MyAPi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAPi v1"));
+
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAPi v1");
 
+                c.OAuthClientId(Configuration["ApiConfiguration:OidcSwaggerUIClientId"]);
+                c.OAuthAppName(Configuration["ApiConfiguration:ApiName"]);
+            });
+
+            app.UseStaticFiles();
             app.UseRouting();
+            app.UseCors(static policy =>
+            {
+                policy.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+            });
 
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
